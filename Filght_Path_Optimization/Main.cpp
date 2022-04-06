59e37a0a604c8b53bbf6e46b7b93391968ae0bdd
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-04-07 00:50:28 +0900

    .
    .

--- Filght_Path_Optimization/Main.cpp
+++ Filght_Path_Optimization/Main.cpp
@@ -89,7 +89,7 @@ int main(){
             // 1. Dijkstra: without weather
                 //Dijkstra
                 adjList.ShowList();
-                adjList.Dijkstra(start_id, end_id);
+                adjList.Dijkstra(start_id);
                 // print result
                 cout << "Shortest path (not consider weather): ";
                 shortestPath_without_weather=adjList.path_queue(start_id, end_id);
@@ -101,7 +101,7 @@ int main(){
                 CheckEdgeAvailable(&adjList, airportList,sectionList, sectionCnt, edgeList, edgeCnt);
                 adjList.ShowList();
                 // Dijkstra
-                adjList.Dijkstra(start_id, end_id);
+                adjList.Dijkstra(start_id);
                 // print result
                 cout << "Shortest path (consider weather): ";
                 shortestPath_weather = adjList.path_queue(start_id, end_id);
--- Filght_Path_Optimization/README.md
+++ Filght_Path_Optimization/README.md
@@ -1,4 +1,12 @@
 # Project01_Flight Path Optimization using Dijkstra’s Algorithm
 -------------
 * notion: https://www.notion.so/Flight-Path-Optimization-52e4cd771bda4b47ab325f1245b1f2f7
-asdfsdf
+
+
+
+컴퓨터 구성 안내
+설치 안내
+사용법
+파일 메니페스트 (파일 목록 포함)
+저작권 및 사용권 정보
+배포자 및 프로그래머의 연락처 정보
