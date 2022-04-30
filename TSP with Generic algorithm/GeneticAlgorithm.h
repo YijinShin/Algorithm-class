b50f32b7b58920f8ace227bc1e8e10f75f17c547
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-05-01 00:13:32 +0900

    crossover 합침

--- TSP with Generic algorithm/GeneticAlgorithm.h
+++ TSP with Generic algorithm/GeneticAlgorithm.h
@@ -26,6 +26,7 @@ struct cmp{
 class GeneticAlgorithm{
     private:
         int deliveryLocationNum = 0;
+        int startPoint;
         vector<Individual> ParentSet;
         vector<Individual> population; 
         priority_queue<IndividualInfo, vector<IndividualInfo>, cmp> populationInfo;
@@ -135,11 +136,10 @@ class GeneticAlgorithm{
                     ParentSet.push_back(population[indexList[i+1]]); 
             }
             cout << "------- Parent set--------\n";
-            PrintIndividualSet(ParentSet);
+            //PrintIndividualSet(ParentSet);
         }
         
         // Roulette wheel Selection 
-
 
         // Elitism
         void Elitism(int percent){
@@ -150,7 +150,7 @@ class GeneticAlgorithm{
             int eliteIndex;
             IndividualInfo info;
             
-            cout << "----- new generation(elite only): "<< population.size()<<"/"<<percent<<"%("<<eliteNum <<")--------\n";
+            cout << "-----New generation(after Elite): "<< population.size()<<"/"<<percent<<"%("<<eliteNum <<")--------\n";
             
             for(int i=0;i<eliteNum;i++){
                 // save elite solution to elite vector
@@ -173,23 +173,135 @@ class GeneticAlgorithm{
             eliteSet.swap(population); // swap delete previous vector. 
             swap(populationInfo, eliteInfo);
 
-            PrintIndividualSetWithInfo(population, populationInfo);
+            //PrintIndividualSetWithInfo(population, populationInfo);
         }
 
         #pragma endregion
 
         //reproduction
-        //replacement
+        #pragma region Reproduction
+        void Reproduction(){
+            for(int i=0;i<ParentSet.size()/2;i++){
+                Crossover(population[2*i].array, population[2*i+1].array);
+            }
+            cout<<"-----------New generation(after Reproduction)-----------\n";
+            //PrintIndividualSetWithInfo(population, populationInfo);
+            PrintIndividualSet(population);
+            
+
+        }
+
+        void Crossover(vector<int> a, vector<int> b) {
+            vector<int> a_child = a;
+            vector<int> b_child = b;
+            queue<int> a_range;//swap 전 해당 구간의 a 원소
+            queue<int> b_range;//swap 전 해당 구간의 b 원소
+            vector<int> a_exist; //b에 이미 존재했던 a_range의 원소의 index
+            vector<int> b_exist; //a에 이미 존재했던 b_range의 원소의 index
+
+            int start = 3; // 논문 예시처럼 하는 거라 이해하기 편하실 거에요
+            int end = 6;
+
+            cout << "strat: " << start << "end: " << end << "\n";
+
+            cout << "a_array: ";
+            for (int i = 0; i < a.size(); i++) {
+                cout << a[i] << " ";
+            }
+            cout << "\n";
+
+            cout << "b_array: ";
+            for (int i = 0; i < b.size(); i++) {
+                cout << b[i] << " ";
+            }
+            cout << "\n";
+
+
+            //해당 구간에 대한 swaping
+            for (int i = start; i < end; i++) {
+                a_range.push(a_child[i]);
+                b_range.push(b_child[i]);
+                swap(a_child[i], b_child[i]);
+            }
+
+            while (a_range.size() != 0 || b_range.size() != 0) {
+                int a_front = a_range.front();
+                int b_front = b_range.front();
+                a_range.pop();
+                b_range.pop();
+
+                for (int i = 0; i < deliveryLocationNum; i++) {
+                    if (i >= start && i < end) continue; //교환한 구간의 원소는 건너뛰기
+                    if (a_front == b_child[i]) {
+                        a_exist.push_back(i);
+                    }
+                    if (b_front == a_child[i]) {
+                        b_exist.push_back(i);
+                    }
+                }
+            }
+            // 기존의 ordering을 유지하기 위해 sort
+            sort(a_exist.begin(), a_exist.end());
+            sort(b_exist.begin(), b_exist.end());
+
+            for (int i = 0; i < a_exist.size(); i++) {
+                a_child[b_exist[i]] = b[a_exist[i]];
+                b_child[a_exist[i]] = a[b_exist[i]];
+            }
+
+            cout << "a'_array: ";
+            for (int i = 0; i < a.size(); i++) {
+                cout << a_child[i] << " ";
+            }
+            cout << "\n";
+
+            cout << "b'_array: ";
+            for (int i = 0; i < b.size(); i++) {
+                cout << b_child[i] << " ";
+            }
+            cout << "\n";
+
+            //Probabilistic Mutations 
+            
+
+
+            // save new child to population(next generation)
+            Individual c1, c2;
+            IndividualInfo c1Info, c2Info;
+                //child a
+            c1.array = a_child;
+            c1.fitness = CalcFitness(a_child);
+            population.push_back(c1);
+            c1Info.index = population.size()-1;
+            c1Info.fitness = c1.fitness;
+            populationInfo.push(c1Info);
+                //child b
+            c2.array = b_child;
+            c2.fitness = CalcFitness(b_child);
+            population.push_back(c2);
+            c2Info.index = population.size()-1;
+            c2Info.fitness = c1.fitness;
+            populationInfo.push(c2Info);
+        }
+
+        void Mutation(vector<int> a){
+            
+        }
+        #pragma endregion
 
     public:
         // generic main function
-        void Genetic(int locNum, double **matrix){
+        void Genetic(int locNum, double **matrix, int start){
             deliveryLocationNum = locNum;
             adjMatrix = matrix;
+            startPoint = start;
             //create initial population
             CreateInitialPopulation();
             //selection
             Selection();
+            //reproduction
+            Reproduction();
+
         }
 
 };
